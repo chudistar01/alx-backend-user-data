96b89b4f3a3fe4b525fcea9f35cf2875e1e58375
chudistar01 <christianigwebuike31@gmail.com>
2024-09-19 01:30:36 +0000

    api

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,8 +12,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """auth function
         """
+        if path is None:
+            return True
 
-        return False
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        normalized_path = path.rstrip('/')
+
+        for excluded in excluded_paths:
+            normalized_excluded = excluded.rstrip('/')
+            if normalized_path == normalized_excluded:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header
