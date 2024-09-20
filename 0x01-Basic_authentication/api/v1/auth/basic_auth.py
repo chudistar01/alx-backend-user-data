1095f7b9ca9dc5bb9c018d4e973297ef1ad665c6
chudistar01 <christianigwebuike31@gmail.com>
2024-09-20 06:31:06 +0000

    api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basicauth file
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """base authorization header
+        """
+        if not base64_authorization_header:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_strings = decoded_bytes.decode('utf-8')
+            return decoded_strings
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
