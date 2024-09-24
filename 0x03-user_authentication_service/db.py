9432320069e630f3cdaf3c9d8573e6aa3a99c0f8
chudistar01 <christianigwebuike31@gmail.com>
2024-09-24 17:14:21 +0000

    user_authentication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,7 @@
 """DB module
 """
+import logging
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,6 +11,8 @@ from user import Base, User
 from typing import Dict
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+
+logging.disable(logging.WARNING)
 
 
 class DB:
