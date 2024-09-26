1839d780e8f29f7db8c17dbf1f87fc273e13f0bc
chudistar01 <christianigwebuike31@gmail.com>
2024-09-26 23:57:46 +0000

    user_authentication_service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,7 +24,7 @@ def index() -> str:
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
     """POST /users                                                                                                                            
-    Rturns: JSON Messsages
+    Returns: JSON Messsages
     """
     email, password = request.form.get("email"), request.form.get("password")
 
@@ -33,6 +33,25 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """POST /users                                                                                                                                  
+    Returns: JSON Messsages  
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = jsonify({"email": email, "message": "logged in"})
+
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
