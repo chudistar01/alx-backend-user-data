dffc2e9ad923ed71a53c7a39325bcfd8f0cd7900
chudistar01 <christianigwebuike31@gmail.com>
2024-09-26 13:52:13 +0000

    user_authentication_service

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Simple Flask APP
+"""
+import logging
+
+from flask import Flask, abort, jsonify, redirect, request
+
+from auth import Auth
+
+logging.disable(logging.WARNING)
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """GET /
+    Rturns: JSON Messsages
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5010")
