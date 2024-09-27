05add416d270b1a274216ae42a16d225a8449cc2
chudistar01 <christianigwebuike31@gmail.com>
2024-09-27 00:29:12 +0000

    user_authentication_service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,18 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Retrieve user object
+        Args:
+           session_id(str): session id
+        Returns:
+           Union[User, None]: user object
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
