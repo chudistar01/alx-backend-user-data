be19184d02a458e0d8d27fe196171ded7fd64910
chudistar01 <christianigwebuike31@gmail.com>
2024-11-07 13:15:03 +0000

    session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -14,17 +14,18 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth_type = os.getenv('AUTH_TYPE')
-
-if auth_type == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-elif auth_type == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(401)
