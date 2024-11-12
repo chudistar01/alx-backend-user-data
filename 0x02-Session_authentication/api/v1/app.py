a499ac05bcf3b60ec4fd668d048fcd5a724c944c
chudistar01 <christianigwebuike31@gmail.com>
2024-11-12 10:22:24 +0000

    session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -14,7 +14,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-AUTH_TYPE = os.getenv('AUTH_TYPE')
+AUTH_TYPE = os.getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,12 @@
 """
 Definition of class SessionAuth
 """
+import base64
+from uuid import uuid4
+from typing import TypeVar
 
 from .auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
